1365b5b762f9a891fb6f7da6f59413b58bbcc0f3
Harrison Melton <hmmelton95@gmail.com>
2023-03-30 14:28:17 -0700

    added Hilt and Application class

---
+++ app/src/main/java/com/hmmelton/bytechef/ByteChefApplication.kt
@@ -0,0 +1,19 @@
+package com.hmmelton.bytechef
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class ByteChefApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
