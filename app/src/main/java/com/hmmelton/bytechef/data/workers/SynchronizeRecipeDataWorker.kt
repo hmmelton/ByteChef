46bc186c226a04752c7b873626ecf61dfeb4a9c4
Harrison Melton <hmmelton95@gmail.com>
2023-04-02 18:50:00 -0700

    added worker for recipe data sync

--- app/src/main/java/com/hmmelton/bytechef/data/workers/SynchronizeRecipeDataWorker.kt
+++ app/src/main/java/com/hmmelton/bytechef/data/workers/SynchronizeRecipeDataWorker.kt
@@ -1,4 +1,43 @@
 package com.hmmelton.bytechef.data.workers
 
-class SynchronizeRecipeDataWorker {
+import android.content.Context
+import android.util.Log
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.hmmelton.bytechef.data.repositories.RecipeRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.coroutineScope
+
+private const val TAG = "SynchronizeUserDataWorker"
+
+/**
+ * Worker class for synchronizing [RecipeRepository] data sources.
+ */
+@HiltWorker
+class SynchronizeRecipeDataWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workParams: WorkerParameters,
+    private val recipeRepository: RecipeRepository
+) : CoroutineWorker(appContext, workParams) {
+
+    override suspend fun doWork() = coroutineScope {
+        try {
+            val uid = inputData.getString(WorkKeys.UID) ?: throw MissingKeyException(WorkKeys.UID)
+            // Attempt to force refresh/sync recipe data
+            // TODO: make more robust sync that doesn't always give priority to remote data source
+            recipeRepository.forceRefreshRecipes(uid)
+            Result.success()
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to complete work", e)
+
+            // If the exception was raised due to a missing input key, do not retry
+            if (e is MissingKeyException) {
+                Result.failure()
+            } else {
+                Result.retry()
+            }
+        }
+    }
 }
